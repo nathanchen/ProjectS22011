7724d60ae0813599d295db54491ab606c8e27f41
Nathan CHEN <natechen@wifi-qche7764.cs.usyd.edu.au>
2011-08-05 16:27:15 +1000

    Initial Commit

---
+++ ProjectS22011/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ProjectS22011
+//
+//  Created by Nathan CHEN on 5/08/11.
+//  Copyright 2011 The University of Sydney. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
