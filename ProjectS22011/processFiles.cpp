54af6e6bde03bd0c63f935843a0080f633c2f710
Nathan CHEN <natechen@Nathans-MacBook-Pro.local>
2011-08-06 23:51:28 +1000

    6/8

---
+++ ProjectS22011/processFiles.cpp
@@ -0,0 +1,14 @@
+//
+//  processFiles.cpp
+//  ProjectS22011
+//
+//  Created by Nathan CHEN on 6/08/11.
+//  Copyright 2011 The University of Sydney. All rights reserved.
+//
+
+#include "processFiles.h"
+
+void selectKeyFrames (char* videoFileDirectory, char* xmlFile)
+{
+    
+}
